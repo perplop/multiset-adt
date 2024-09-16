6640d0b270df83a19f853f2a17066570152015c1
perplop <andychen3870@gmail.com>
2024-09-16 14:47:18 -0400

    completed TreeMultiSet class

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -3,12 +3,40 @@
  */
 public abstract class MultiSet {
 
-    /**
-     * Add the given item to this multiset.
-     *
-     * @param item the item to add
-     */
     abstract void add(Integer item);
 
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 }
+public class TreeMultiSet(Multiset){
+    /*
+    This class uses an underlying Tree to implement our MultiSet ADT.
+    */
+    Tree _tree;
+    public TreeMultiSet(Tree _tree){
+        this._tree = _tree;
+    }git
+
+    public void add(int item){
+        _tree.insert(item);
+    }
+    public void remove(int item){
+        _tree.delete_item(item);
+    }
+    public boolean contains(int item){
+        return this._tree.__contains__(item);
+    }
+    public boolean is_empty(){
+        return this._tree.is_empty();
+    }
+    public int count(int item){
+        return this._tree.count(item);
+    }
+    public int size(){
+        return this._tree.__len__();
+    }
+
+
+
+
+}
+
+
